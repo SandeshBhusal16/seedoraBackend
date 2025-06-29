21501a64d82660fb95178e98284dc03e59b1c197
Sandesh Bhusal <sandeshbhusal417@gmail.com>
2025-06-29 18:35:38 +0545

    fix: database inserOne buffering timout problem

--- config/database.config.js
+++ config/database.config.js
@@ -11,14 +11,7 @@ if (process.env.NODE_ENV === "production") {
 // console.log(process.env);
 
 mongoose
-  .connect(process.env.DB_URL, {
-    autoCreate: true,
-    autoIndex: false,
-    serverSelectionTimeoutMS: 50000, // Set server selection timeout to 50 seconds
-    socketTimeoutMS: 45000, // Set socket timeout to 45 seconds
-    retryWrites: true,
-    w: "majority",
-  })
+  .connect(process.env.DB_URL, {})
   .then((req) => {
     console.log("Database Connected Succussfully");
   })
